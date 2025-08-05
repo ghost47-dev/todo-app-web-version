bb56edde26db441e00da0f7aec3106279c6a988b
ghost47 <robertpetit730@gmail.com>
2025-08-05 08:13:00 +0100

    Added a way to mark tasks as done or undone

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function to_dict(title, priority, date, status) {
+function toDict(title, priority, date, status) {
     let task = {
         "title": title,
         "priority": priority,
@@ -8,7 +8,7 @@ function to_dict(title, priority, date, status) {
     return task;
 }
 tasks = []
-function add_task() {
+function addTask() {
     let title = document.getElementById("task-title").value;
     let priority = document.getElementById("task-priority").value;
     let date = document.getElementById("task-date").value;
@@ -19,19 +19,48 @@ function add_task() {
     }
 
     console.log(title, priority, date);
-    task = to_dict(title, priority, date, false);
+    task = toDict(title, priority, date, false);
     tasks.push(task);
-    display_tasks();
+    displayTasks();
 }
 
-function display_tasks() {
+function displayTasks() {
     const ulElement = document.getElementById("task-list");
     ulElement.innerHTML = "";
-    tasks.forEach(element => {
+
+    tasks.forEach((element, index) => {
         const newLi = document.createElement('li');
-        newLi.innerHTML = `${element['title']} - <b>P${element['priority']}</b> due for ${element['date']}`;
+        const checkbox = document.createElement('input');
+        const span = document.createElement('span');
+
+        checkbox.type = "checkbox";
+        checkbox.setAttribute("data-index", index);
+        checkbox.checked = element.status;
+
+        span.innerHTML = ` ${element['title']} - <b>P${element['priority']}</b> due for ${element['date']}`;
+        span.style.textDecoration = element.status ? "line-through" : "none";
+
+        newLi.appendChild(checkbox);
+        newLi.appendChild(span);
+
         ulElement.appendChild(newLi);
+
+        document.querySelectorAll("ul li input").forEach(input => {
+            input.addEventListener("change", (e) => {
+                const index = e.target.getAttribute("data-index");
+                console.log(index);
+                tasks[index].status = e.target.checked;
+
+                const span = e.target.nextSibling;
+                if (e.target.checked) 
+                    span.style.textDecoration = "line-through";
+                else
+                    span.style.textDecoration = "none";
+                
+            })
+        })
     });
 }
 
-document.getElementById("add-task-btn").addEventListener("click", add_task);
+document.getElementById("add-task-btn").addEventListener("click", addTask);
+
